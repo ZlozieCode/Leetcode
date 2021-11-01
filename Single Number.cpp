f71b16a14537fbc6378f76ea2fa0c449c4808d45
ChattyMinado <92716726+ChattyMinado@users.noreply.github.com>
2021-11-01 11:58:22 +0530

    Create Single Number.cpp

---
+++ Single Number.cpp
@@ -0,0 +1,19 @@
+// q-> https://leetcode.com/problems/single-number/submissions/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        unordered_map<int,int>m;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        
+        for(auto x:m){
+            if(x.second==1)return x.first;
+        }
+        
+        return -1;
+    }
+};
